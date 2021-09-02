b8ed76f97b814f4176ccfabd3dd698e16bfbfae4
Craig McKeachie <craigdmckeachie@gmail.com>
2021-09-02 14:17:57 -0400

    reducer compiling

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,10 +6,15 @@ import {
   MetaReducer,
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { projectReducer, ProjectState } from '../projects/shared/state/project.reducer';
 
-export interface State {}
+export interface State {
+  projectState: ProjectState
+}
 
-export const reducers: ActionReducerMap<State> = {};
+export const reducers: ActionReducerMap<State> = {
+  projectState: projectReducer
+};
 
 export const metaReducers: MetaReducer<State>[] = !environment.production
   ? []
