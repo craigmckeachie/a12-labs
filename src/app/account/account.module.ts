5f1967533bef664eb5878d087f05130468ebe069
Craig McKeachie <craigdmckeachie@gmail.com>
2021-09-05 17:10:40 -0400

    account header

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AccountHeaderComponent } from './account-header/account-header.component';
 
 @NgModule({
-  declarations: [SigninComponent],
+  declarations: [SigninComponent, AccountHeaderComponent],
   imports: [CommonModule, AccountRoutingModule, ReactiveFormsModule],
+  exports: [AccountHeaderComponent],
 })
 export class AccountModule {}
