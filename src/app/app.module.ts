d02eb1249deb5cf3de84f818967f5fd538eaa483
Craig McKeachie <craigdmckeachie@gmail.com>
2021-09-05 17:51:41 -0400

    auth0 library working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { HomeModule } from './home/home.module';
 import { AccountModule } from './account/account.module';
 import { JwtModule } from '@auth0/angular-jwt';
 import { AuthInterceptor } from './core/auth.interceptor';
+import { UserService } from './account/shared/user.service';
 
 export function tokenGetter() {
-  return localStorage.getItem('access_token');
+  return UserService.authorizationToken ?? '';
 }
 
 @NgModule({
@@ -23,20 +24,20 @@ export function tokenGetter() {
     HttpClientModule,
     HomeModule,
     AccountModule,
-    // JwtModule.forRoot({
-    //   config: {
-    //     tokenGetter: tokenGetter,
-    //     allowedDomains: ['localhost:3000'],
-    //     disallowedRoutes: [
-    //       // '//localhost:4200/home/',
-    //       // '//localhost:4200/account/',
-    //     ],
-    //   },
-    // }),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ['localhost:4200', 'localhost:3000'],
+        disallowedRoutes: [
+          // '//localhost:4200/home/',
+          // '//localhost:4200/signin/',
+        ],
+      },
+    }),
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  ],
+  // providers: [
+  //   { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  // ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
